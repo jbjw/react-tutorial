76103a44dacd770a85dddce20278647b730fa5c3
jbjw <github@jbjw.com>
2017-08-24 17:35:43 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// var webpack = require('webpack');
+var path = require('path')
+
+const path = require('path')
+
+const DIST = path.resolve(__dirname, 'dist')
+const SRC = path.resolve(__dirname, 'src')
+
+module.exports = {
+	devtool: 'source-map',
+	devServer: {
+		inline: true,
+		// hot: true,
+		contentBase: 'dist/',
+		port: 80,
+	},
+	entry: path.resolve(DIST, 'main.js'),
+	output: {
+		path: SRC,
+		filename: 'bundle.js',
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				query: {
+					presets: ['es2015', 'react'],
+				},
+			},
+		],
+	},
+}
